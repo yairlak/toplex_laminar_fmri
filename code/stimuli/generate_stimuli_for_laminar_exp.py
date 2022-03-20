fed4b49b0962eb39bf12749e14e6b88146adc42f
Yair LAKRETZ <yair.lakretz@gmail.com>
2022-03-20 13:27:15 +0100

    first commit

--- code/stimuli/generate_stimuli_for_laminar_exp.py
+++ code/stimuli/generate_stimuli_for_laminar_exp.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # LOAD LEXIQUE
-fn = 'Lexique383.tsv'
+fn = '../../data/lexique/Lexique383.tsv'
 df = pd.read_csv(fn, delimiter='\t')
 
 # CONDITIONS
@@ -31,14 +31,14 @@ log_freqlivres_perc_10 = np.percentile(df['log_freqlivres'], 10)
 # QUERY DF HIGH FREQ
 df_high = df.loc[df['log_freqlivres']>log_freqlivres_perc_90]
 df_high = df_high.query(' & '.join(conditions))
-df_high.to_csv('high_freq.tsv', sep='\t')
+df_high.to_csv('../../stimuli/high_freq.tsv', sep='\t')
 print('HIGH FREQ')
 print(sorted(df_high['ortho'].values))
 
 # QUERY DF LOW FREQ
 df_low = df.loc[df['log_freqlivres']<log_freqlivres_perc_10]
 df_low = df_low.query(' & '.join(conditions))
-df_low.to_csv('low_freq.tsv', sep='\t')
+df_low.to_csv('../../stimuli/low_freq.tsv', sep='\t')
 print('LOW FREQ')
 print(sorted(df_low['ortho'].values))
 
